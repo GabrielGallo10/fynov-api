4e7ce94f3389db4080359e53a94d486ab459faaf
GabrielGallo <gabrielgallov@gmail.com>
2026-05-20 14:18:33 -0300

    Ajuste simples em AtualizarUsuarioRequest

--- src/main/java/br/com/fiap/fynov/dto/AtualizarUsuarioRequest.java
+++ src/main/java/br/com/fiap/fynov/dto/AtualizarUsuarioRequest.java
@@ -2,7 +2,6 @@ package br.com.fiap.fynov.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 public record AtualizarUsuarioRequest(
     @NotBlank(message = "O nome nao pode ser vazio")
